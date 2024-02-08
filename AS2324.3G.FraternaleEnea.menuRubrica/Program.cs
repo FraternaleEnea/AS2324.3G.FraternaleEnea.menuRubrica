cf31f3723a20b9f6ad8b815874e4fe36a62252e8
PC05 <PC05@INFO5-2802>
2024-02-08 09:29:57 +0100

    Aggiungere i file di progetto.

---
+++ AS2324.3G.FraternaleEnea.menuRubrica/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2324._3G.FraternaleEnea.menuRubrica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
